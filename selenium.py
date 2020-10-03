58177f00f8b464e2dd5aaa64ad8f5663bc9ab4f9
omar-havef <68714153+omar-havef@users.noreply.github.com>
2020-10-03 12:36:32 +0200

    Create selenium.py

---
+++ selenium.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from time import sleep
+import os
+from sys import argv
+
+url = argv[1]
+
+options = webdriver.ChromeOptions()
+
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.headless = True
+
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(2000, 2000)
+driver.implicitly_wait(10)
+
+driver.get("https://anonfiles.com/LfUap8X4o0/WinPE10_8_Sergei_Strelec_x86_x64_2020.09.21_English_rar/")
+
+download = driver.find_element_by_id("download-url")
+download_link = download.get_attribute("href")
+
+print("Download Link: ", download_link)
+
+os.system("wget " + download_link)
+
+driver.quit()
